28b9b690d869d4b72d5159c96f1ea5062780e893
unknown <girdharshukla01@gmail.com>
2022-02-03 21:14:26 +0530

    Base

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Greeting Master!')
+
+
+client.run(os.environ.get('TOKEN'))
